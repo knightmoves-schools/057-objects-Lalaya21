2ceca5ddbde0638c5b2b74f652b8b67b0a13cd3c
lalaya21 <lalaya.shuckahosee@students.msswarriors.org>
2026-04-27 19:30:06 +0000

    done1

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const school = {
+    name: "Westview High",
+    averageClassSize: 25,
+    mascot: "Wildcat"
+};
 
+const description = `${school.name} ${school.averageClassSize} ${school.mascot}`;
+document.getElementById("result").innerHTML = description;
