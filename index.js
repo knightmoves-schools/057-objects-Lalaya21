4bf30aa02cf804fc4e9f650b0b8a72d0bf89f733
lalaya21 <lalaya.shuckahosee@students.msswarriors.org>
2026-04-28 17:02:51 +0000

    done2

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const school = {
+const { useLayoutEffect } = require("react");
+
+useLayoutEffect school = {
+useLayoutEffect school = {
     name: "Westview High",
     averageClassSize: 25,
     mascot: "Wildcat"
 };
 
-const description = `${school.name} ${school.averageClassSize} ${school.mascot}`;
+const description = `${school.name}${school.averageClassSize}${school.mascot}`;
 document.getElementById("result").innerHTML = description;
