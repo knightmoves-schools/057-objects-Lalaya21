55dc5f2d6cb102c8def176526f7275f27de25a16
lalaya21 <lalaya.shuckahosee@students.msswarriors.org>
2026-05-01 18:48:38 +0000

    done12

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const `school`= {
     name: 'meskwaki',
     "average class size": '3',
     mascot: 'hawk'
-};d i h 
+};
 Document.getElementById("result").innerHTML = "${name} has an average class size of ${average class size} and their mascot is a ${mascot}";
