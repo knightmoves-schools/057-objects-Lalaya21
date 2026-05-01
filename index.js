28b7362e4bd560235296119716d2e18ea3c7320b
lalaya21 <lalaya.shuckahosee@students.msswarriors.org>
2026-05-01 18:36:51 +0000

    done8

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const school = {
     name: 'meskwaki',
-    average Class Size: 3,
+    average class size: 3,
     mascot: 'hawk'
 };d i h 
-document.getElementById("result").innerHTML = "${school.name} has an average class size of ${school.averageClassSize} and their mascot is a ${school.mascot}";
+document.getElementById("result").innerHTML = "${school.name} has an average class size of ${school.average class size} and their mascot is a ${school.mascot}";
